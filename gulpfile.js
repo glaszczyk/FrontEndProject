11cd25bfe40f49cf61fe1a09e0c2f40397048cdd
glaszczyk <info@grzegorzlaszczyk.pl>
2016-09-02 19:34:10 +0200

    Pierwsze dwa widoki

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+	return gulp.src('./sass/main.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	.pipe(autoprefixer())
+	.pipe(rename('style.css'))
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('./css'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task("default", ["sass"], function(){
+	browserSync.init({
+		server: {
+			baseDir: "./",
+			index: "profile.html"
+		}
+	});
+
+	gulp.watch('./sass/**/*.scss', ['sass']);
+	gulp.watch('./*.html', browserSync.reload);
+});
